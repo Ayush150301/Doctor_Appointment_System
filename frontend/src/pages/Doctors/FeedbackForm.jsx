76fc72bfe2f7feab4529221cb1bc6421ca991068
[firstname <[ayushpandey150301@gmail.com]>
2024-03-08 23:34:01 +0530

    Frontend Completed

--- frontend/src/pages/Doctors/FeedbackForm.jsx
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -4,11 +4,17 @@ import { AiFillStar } from "react-icons/ai";
 const FeedbackForm = () => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [reviewText, setReviewText] = useState("");
+  const handleSubmitReview = async e=>{
+    e.preventDefault()
+
+    //later we use api to set
+  }
 
   return (
     <form action="">
       <div className="">
-        <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4">
+        <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4 mt-0">
           How would your rate the overall experience?*
         </h3>
         <div className="">
@@ -39,6 +45,19 @@ const FeedbackForm = () => {
           })}
         </div>
       </div>
+
+      <div className="mt-[30px]">
+        <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4 mt-0">
+          Share your feedback or suggestions*
+        </h3>
+
+        <textarea className="border border-solid border-[#0066ff34] focus:outline outline-primaryColor w-full  px-4 py-3 rounded-md" rows="5" placeholder="Write your message"
+        onChange={e=>setReviewText(e.target.value)}
+        ></textarea>
+      </div>
+      <button type="submit" 
+      onClick={handleSubmitReview}
+      className="btn">Submit Feedback</button>
     </form>
   );
 };
